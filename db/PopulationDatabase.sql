b2e198c1882ca6ccb3370405c451efe98c556327
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-01-25 16:42:03 +0100

    Created PopulationDatabase.sql. Populated Giocatore table.

---
+++ db/PopulationDatabase.sql
@@ -0,0 +1,8 @@
+INSERT INTO `giocatore`
+VALUES ('S_Nova', 'Ginevra', 'Bartolini', '2002-10-21', 'progetto01');
+
+INSERT INTO `giocatore`
+VALUES ('Tripp', 'Galileo', 'Foschini', '2002-09-15', 'progetto02');
+
+INSERT INTO `giocatore`
+VALUES ('WOC', 'Wizard', 'Of The Coast', '1990-01-01', 'wizardPass');
