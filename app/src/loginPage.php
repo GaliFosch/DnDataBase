50c185cb76e44b8870c5fd3c30e96033ca01225b
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-28 23:01:28 +0100

    updated LoginPage

--- app/src/loginPage.php
+++ app/src/loginPage.php
@@ -1,31 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Login</title>
-</head>
-
-<body>
-    <header>
-        <h1>DnDatabase</h1>
-    </header>
-    <main>
-        <h2>Benvenut*</h2>
-        <form action="#" method="get">
-            <ul>
-                <li>
-                    <label for="nickname">Nickname</label>
-                    <input type="text" name="nickname" id="nickname">
-                </li>
-                <li>
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password">
-                </li>
-            </ul>
-            <input type="submit" value="Login">
-        </form>
-        <p>Non hai un account? <a href="#">Creane uno</a></p>
-    </main>
-</body>
-
-</html>
+<?php 
+require_once("bootstrap.php");
+$template["title"] = "Login";
+$template["file"] = "loginTemplate.php";
+require("template/base.php");
