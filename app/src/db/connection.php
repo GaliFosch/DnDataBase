4352a7b654e820a399b21d4b6da9cbe2ece37217
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-28 22:39:06 +0100

    Created connection.php

---
+++ app/src/db/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+class Connection {
+    private $instance;
+
+    public function __construct(){
+        $this->instance = new mysqli("localhost","root","","dndatabase");
+        if ($this->instance -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $this->instance -> connect_error;
+            exit();
+        }
+    }
+
+    public function getConnection(){
+        return $this->instance;
+    }
+}
