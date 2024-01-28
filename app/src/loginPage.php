abeaaf8adac216ff60014af1d8f4e3dadeba4d0f
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-28 22:25:38 +0100

    Created loginPage

---
+++ app/src/loginPage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <h1>DnDatabase</h1>
+    </header>
+    <main>
+        <h2>Benvenut*</h2>
+        <form action="#" method="get">
+            <ul>
+                <li>
+                    <label for="nickname">Nickname</label>
+                    <input type="text" name="nickname" id="nickname">
+                </li>
+                <li>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                </li>
+            </ul>
+            <input type="submit" value="Login">
+        </form>
+        <p>Non hai un account? <a href="#">Creane uno</a></p>
+    </main>
+</body>
+
+</html>
