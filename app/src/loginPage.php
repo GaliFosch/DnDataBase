5594c93b07b14cbf3273a93ee380e59688f6b5f9
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-29 20:27:19 +0100

    Implemented login function

--- app/src/loginPage.php
+++ app/src/loginPage.php
@@ -1,5 +1,18 @@
 <?php 
+if(isset($_SESSION["user"])){
+    header("Location: http://localhost/DnDataBase/app/src/Hello.html");
+}
 require_once("bootstrap.php");
 $template["title"] = "Login";
 $template["file"] = "loginTemplate.php";
 require("template/base.php");
+
+if(!empty($_GET["nickname"]) && !empty($_GET["password"])){
+    $result = $db->getAccount($_GET["nickname"], $_GET["password"]);
+    if(!$result){
+        $template["log error"] = "Combinazione Nickname e Password errata";
+    }else{
+        $_SESSION["user"] = $result;
+    }
+}
+
