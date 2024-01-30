bd1df7a7b26f859450d4408baf43251cc8149b8d
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-30 11:48:51 +0100

    created regResTempl

---
+++ app/src/template/regResTempl.php
@@ -0,0 +1,6 @@
+<main>
+    <h2>Congratulazioni</h2>
+    <p>Registrazione avvenuta con successo!</p>
+    <br/>
+    <a href="http://localhost/DnDataBase/app/src/loginPage.php">Clicca qui per effettuare il Login</a>
+</main>
