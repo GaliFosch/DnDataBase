8a4dad968f236a3e9d8c583e928984d5a4e9106a
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-01-30 11:59:58 +0100

    Refined register.php

--- app/src/register.php
+++ app/src/register.php
@@ -2,15 +2,22 @@
 require_once("bootstrap.php");
 $template["title"] = "Registration";
 $template["file"] = "registerTemplate.php";
-require("template/base.php");
 
-if(!empty($_GET["name"]) 
-    && !empty($_GET["surname"])
-    && !empty($_GET["birthdate"])
-    && !empty($_GET["nickname"])
-    && !empty($_GET["password"])){
-    $sql = "INSERT INTO giocatore(nome,cognome,data_nascita,nickname,password) VALUES (?,?,?,?,?)";
-    $stmnt = $db->getConnection()->prepare($sql);
-    $stmnt->bind_param("sssss", $_GET["name"], $_GET["surname"], $_GET["birthdate"], $_GET["nickname"], $_GET["password"]);
-    $stmnt->execute();
+if(!empty($_GET["name"])
+&& !empty($_GET["surname"])
+&& !empty($_GET["birthdate"])
+&& !empty($_GET["nickname"])
+&& !empty($_GET["password"])){
+    if(!$db->isPresent($_GET["nickname"])){
+        $sql = "INSERT INTO giocatore(nome,cognome,data_nascita,nickname,password) VALUES (?,?,?,?,?)";
+        $stmnt = $db->getConnection()->prepare($sql);
+        $stmnt->bind_param("sssss", $_GET["name"], $_GET["surname"], $_GET["birthdate"], $_GET["nickname"], $_GET["password"]);
+        $stmnt->execute();
+        $template["title"] = "Congratulazioni";
+        $template["file"] = "regResTempl.php";
+    }else{
+        $template["err_REG"] = "Nickname già preso!";
+    }
 }
+
+require("template/base.php");
