200a7aad1725ec79616611786c55b850bd68867c
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-01-31 21:36:37 +0100

    Created selection css. To be finished.

--- app/src/template/selectionTemplate.php
+++ app/src/template/selectionTemplate.php
@@ -1,12 +1,23 @@
 <main>
-    <?php echo "<h2>Benenut* ".$_SESSION["user"]["Nickname"]."</h2>"?>
-    <p>Scegli da dove cominciare!</p>
-    <div>
-        <img src="#" alt=""/>
-        <a href="#">Player</a>
-    </div>
-    <div>
-        <img src="#" alt=""/>
-        <a href="#">Dungeon Master</a>
+    <div class="selection-wrap">
+        <div class="selection-title">
+            <?php echo "<h1 class='benvenuto-title'>Benvenut* ".$_SESSION["user"]["Nickname"]."</h1>"?>
+            <p>Scegli da dove cominciare!</p>
+        </div>
+        <div class="selection-container">
+            <div class="selection-image player">
+                <a href="#">
+                    <img src="..\..\images\player.jpg" alt=""/>
+                    <p>Player</p>
+                </a>
+            </div>
+            <div class="middle"></div>
+            <div class="selection-image master">
+                <a href="#">
+                    <img src="..\..\images\dungeon-master.jpg" alt=""/>
+                    <p>Dungeon Master</p>
+                </a>
+            </div>
+        </div>
     </div>
 </main>
