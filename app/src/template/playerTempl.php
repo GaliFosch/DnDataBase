b1d54d42fc3d42639aff0bd19603a952d71df324
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-01 15:07:22 +0100

    created scrolling script

--- app/src/template/playerTempl.php
+++ app/src/template/playerTempl.php
@@ -4,34 +4,53 @@
     echo "<h2>".$_SESSION["user"]["Nickname"]."</h2>";
     ?>
 </div>
-
 <main>
-    <div>
+    <div class="scrolling">
         <h3>Diario delle Campagne</h1>
         <!-- Roba dello scorrimento -->
-        <img src="#" alt="freccia sinistra"/>
+        <img class="leftArrow" src="#" alt="freccia sinistra"/>
         <div>
             <img src="#" alt="icona Campagna"/>
-            <p>Nome Campagna</p><br/>
+            <p>1 Campagna</p><br/>
             <p>00/00/00</p>
         </div>
         <div>
             <img src="#" alt="icona Campagna"/>
-            <p>Nome Campagna</p><br/>
+            <p>2 Campagna</p><br/>
             <p>00/00/00</p>
         </div>
         <div>
             <img src="#" alt="icona Campagna"/>
-            <p>Nome Campagna</p><br/>
+            <p>3 Campagna</p><br/>
             <p>00/00/00</p>
         </div>
-        <img src="#" alt="freccia destra"/>
+        <div>
+            <img src="#" alt="icona Campagna"/>
+            <p>4 Campagna</p><br/>
+            <p>00/00/00</p>
+        </div>
+        <div>
+            <img src="#" alt="icona Campagna"/>
+            <p>5 Campagna</p><br/>
+            <p>00/00/00</p>
+        </div>
+        <div>
+            <img src="#" alt="icona Campagna"/>
+            <p>6 Campagna</p><br/>
+            <p>00/00/00</p>
+        </div>
+        <div>
+            <img src="#" alt="icona Campagna"/>
+            <p>7 Campagna</p><br/>
+            <p>00/00/00</p>
+        </div>
+        <img class="rightArrow" src="#" alt="freccia destra"/>
     </div>
-
-    <div>
+    
+    <div class="scrolling">
         <h3>I tuoi Personaggi</h3>
         <!-- Roba dello scorrimento -->
-        <img src="#" alt="freccia sinistra"/>
+        <img class="leftArrow" src="#" alt="freccia sinistra"/>
         <div>
             <img src="#" alt="Icona PG"/>
             <p>Nome personaggio</p><br/>
@@ -44,6 +63,7 @@
             <img src="#" alt="Icona PG"/>
             <p>Nome personaggio</p><br/>
         </div>
-        <img src="#" alt="freccia destra"/>
+        <img class="rightArrow" src="#" alt="freccia destra"/>
     </div>
+    <script src="js/scrolling.js"></script>
 </main>
