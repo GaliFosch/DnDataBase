89f59dc0d0ada72d99c1bb4a0af5b2c3ae3277d5
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-02-04 09:35:02 +0100

    Started creating player interface.

--- app/src/template/playerTempl.php
+++ app/src/template/playerTempl.php
@@ -1,58 +1,64 @@
-<div>
-    <?php
-    //img
-    echo "<h2>".$_SESSION["user"]["Nickname"]."</h2>";
-    ?>
-</div>
-<main>
-    <div class="scrolling">
-        <h3>Diario delle Campagne</h1>
-        <img class="leftArrow" src="#" alt="freccia sinistra"/>
+<main>   
+    <div class="container user">
         <?php
-            $sql = "SELECT nome, immagine
-                    FROM Campagna
-                    WHERE Id_campagna IN (SELECT Id_campagna
-                                        FROM Eroe INNER JOIN Pg ON Eroe.IDPersonaggio = Pg.IDPersonaggio
-                                        WHERE Pg.Creatore = ?)";
-            $stmnt = $db->getConnection()->prepare($sql);
-            $stmnt->bind_param("s", $_SESSION["user"]["Nickname"]);
-            $stmnt->execute();
-            $result = $stmnt->get_result();
+        //img
+        echo "<img>".$_SESSION["user"]["Immagine"]."</img>";
+        echo "<h2>".$_SESSION["user"]["Nickname"]."</h2>";
         ?>
-        <?php while($row = $result->fetch_assoc()) {?>
-            <div>
-                <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
-                <p><?php echo $row['Nome']; ?></p>
-            </div>
-        <?php }?>
-        <img class="rightArrow" src="#" alt="freccia destra"/>
+        <p>Giocatore</p>
     </div>
-    
-    <div class="scrolling">
-        <h3>I tuoi Personaggi</h3>
-        <img class="leftArrow" src="#" alt="freccia sinistra"/>
-        <div>
-            <a href="#">
-                <img src="#" alt=""/>
-                <p>Nuovo Personaggio</p>
-            </a>
+
+        <div class="container scrolling campaign">
+            <h3>Diario delle Campagne</h3>
+            <img class="leftArrow" src="#" alt="freccia sinistra"/>
+            <button class="arrow sx"></button>
+            <?php
+                $sql = "SELECT nome, immagine
+                        FROM Campagna
+                        WHERE Id_campagna IN (SELECT Id_campagna
+                                            FROM Eroe INNER JOIN Pg ON Eroe.IDPersonaggio = Pg.IDPersonaggio
+                                            WHERE Pg.Creatore = ?)";
+                $stmnt = $db->getConnection()->prepare($sql);
+                $stmnt->bind_param("s", $_SESSION["user"]["Nickname"]);
+                $stmnt->execute();
+                $result = $stmnt->get_result();
+            ?>
+            <?php while($row = $result->fetch_assoc()) {?>
+                <div class="wrap">
+                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
+                    <p><?php echo $row['Nome']; ?></p>
+                </div>
+            <?php }?>
+            <button class="arrow dx"></button>
+            <img class="rightArrow" src="#" alt="freccia destra"/>
         </div>
-        <?php
-            $sql = "SELECT Nome, Immagine
-                    FROM Pg
-                    WHERE Creatore = ?";
-            $stmnt = $db->getConnection()->prepare($sql);
-            $stmnt->bind_param("s", $_SESSION["user"]["Nickname"]);
-            $stmnt->execute();
-            $result = $stmnt->get_result();
-        ?>
-        <?php while($row = $result->fetch_assoc()) {?>
-            <div>
-                <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
-                <p><?php echo $row['Nome']; ?></p>
+        
+        <div class="container scrolling pg">
+            <h3>I tuoi Personaggi</h3>
+            <button class="arrow sx"></button>
+            <img class="leftArrow" src="#" alt="freccia sinistra"/>
+            <div class="wrap">
+                <a href="#">
+                    <img src="..\..\images\plus-sign.jpg" alt=""/>
+                    <p>Nuovo Personaggio</p>
+                </a>
             </div>
-        <?php }?>
-        <img class="rightArrow" src="#" alt="freccia destra"/>
-    </div>
-    <script src="js/scrolling.js"></script>
-</main>
+            <?php
+                $sql = "SELECT Nome, Immagine
+                        FROM Pg
+                        WHERE Creatore = ?";
+                $stmnt = $db->getConnection()->prepare($sql);
+                $stmnt->bind_param("s", $_SESSION["user"]["Nickname"]);
+                $stmnt->execute();
+                $result = $stmnt->get_result();
+            ?>
+            <?php while($row = $result->fetch_assoc()) {?>
+                <div class="wrap">
+                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
+                    <p><?php echo $row['Nome']; ?></p>
+                </div>
+            <?php }?>
+            <button class="arrow dx"></button>
+            <img class="rightArrow" src="#" alt="freccia destra"/>
+        </div>
+        <script src="js/scrolling.js"></script>
