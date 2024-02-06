39fbbd7ca1a14d7edcecc31d2876b22e7d7071bf
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-02-06 18:10:59 +0100

    started crwting the character view.

---
+++ app/src/template/characterView.php
@@ -0,0 +1,52 @@
+<main>
+    <div class="container">
+
+        <div class="top">
+
+            <div class="pg">
+                <img src="..\..\..\images\pg\William DeFoe.jpg" alt="Immagine Personaggio">
+                <div class="info">
+                    <h2>Nome</h2>
+                    <p class="class">Classe, livello</p>
+                    <p class="alignment">Allineamento, taglia</p>
+                    <p class="creatore">Creatore</p>
+                </div>
+            </div> 
+
+            <div class="CAPF">
+                <div class="CA">
+                    <p>CA:</p><p>Numero</p>
+                </div>
+                <div class="PF">
+                    <p>PF:</p> <p>Numero</p>
+                </div>
+            </div> 
+
+        </div> 
+        
+        <div class="desc">
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin tristique orci, ut suscipit massa hendrerit vel. In ultrices justo in justo ornare imperdiet. Vivamus turpis nunc, facilisis vitae vestibulum vel, scelerisque nec urna. In lorem eros, efficitur nec nisl in, mattis imperdiet tellus. Donec ante purus, faucibus sed viverra non, faucibus venenatis tellus. Quisque sit amet scelerisque justo, vel ultrices nulla. Duis ullamcorper ultrices nulla, sit amet interdum tellus viverra non. Fusce dictum vel ligula vel tincidunt. Donec quis suscipit dui. </p>
+        </div> 
+
+        <div class="stats">
+            <div class="stat wrap">
+                <p class="statistica">Statistica</p>
+                <p class="numero">Numero</p>
+                <p class="mod">Mod</p>
+            </div>
+        </div> 
+
+        <div class="points">
+            <p>Percezione passiva</p>
+            <p>Bonus competenza</p>
+        </div> 
+
+        <div class="equipment">
+            <ul>
+                <li>Spada</li>
+                <li>Torcia</li>
+            </ul>
+        </div> 
+
+    </div>    
+</main>
