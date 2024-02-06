a607368cc4ad7168fa932c902012861649560adf
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-06 16:19:33 +0100

    finished base for list page

--- app/src/list.php
+++ app/src/list.php
@@ -10,6 +10,7 @@ if(empty($_GET["listName"])){
         case "specie":
             $template["title"] = "Specie";
             $template["file"] = "listTempl.php";
+            $template["idname"] = "IdSpecie"; 
             $template["columns"] = ["Nome", "Creatore"];
             break;
         default:
--- app/src/template/listTempl.php
+++ app/src/template/listTempl.php
@@ -9,22 +9,27 @@
         </thead>
         <tbody>
             <?php
-                $sql = "SELECT " . $template["columns"][0];
+                $sql = "SELECT ". $template["idname"] . ", " . $template["columns"][0];
                 for($i = 1; $i<count($template["columns"]); $i++){
                     $sql = $sql . ", " . $template["columns"][$i];
                 }
                 $sql = $sql . " FROM " . $template["title"];
                 $result = $db->getConnection()->query($sql);
-                while($row = $result->fetch_array(MYSQLI_NUM)){
+                while($row = $result->fetch_assoc()){
             ?>
                 <tr>
-                    <?php
-                        foreach($row as $val){
-                            echo "<td>".$val."</td>";
-                        }
-                    ?>
+                    <?php foreach($template["columns"] as $col){ ?>
+                        <td>
+                            <a href="<?php echo "http://localhost/DnDataBase/app/src/species.php?id=" . $row[$template["idname"]] . "#"?>" target="visualizer">
+                            <?php echo $row[$col]?>
+                            </a>
+                        </td>
+                    <?php }?>
                 </tr>
             <?php }?>
         </tbody>
     </table>
 </main>
+<aside>
+    <iframe src="http://localhost/DnDataBase/app/src/Hello.html" name="visualizer" frameborder="0"></iframe>
+</aside>
