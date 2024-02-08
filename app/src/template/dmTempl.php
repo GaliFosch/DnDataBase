ed416272f6897d3a57b917ea57b0217745b25732
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-02-08 21:35:10 +0100

    fixed dm

--- app/src/template/dmTempl.php
+++ app/src/template/dmTempl.php
@@ -1,22 +1,24 @@
-<div>
-    <?php
-    //img
-    echo "<h2>".$_SESSION["user"]["Nickname"]."</h2>";
-    ?>
-</div>
-
 <main>
-    <div class="scrolling">
+    
+    <div class="container user">
+        <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($_SESSION['user']['Immagine']); ?>" />
+        <?php
+        echo "<h2>".$_SESSION["user"]["Nickname"]."</h2>";
+        ?>
+        <p>Dungeon Master</p>
+    </div>
+
+    <div class="container scrolling campaign">
         <h3>Diario delle Campagne Create</h3>
-        <img class="leftArrow" src="#" alt="freccia sinistra"/>
-        <div>
+        <button class="arrow sx"></button>
+        <div class="wrap">
             <a href="#">
-                <img src="#" alt=""/>
+                <img src="..\..\images\plus-sign.jpg" alt=""/>
                 <p>Nuova Campagna</p>
             </a>
         </div>
         <?php
-            $sql = "SELECT Nome, Immagine
+            $sql = "SELECT Nome, Immagine, Id_campagna
                     FROM Campagna
                     WHERE Creatore = ?";
             $stmnt = $db->getConnection()->prepare($sql);
@@ -25,12 +27,15 @@
             $oggetto = $stmnt->get_result();
         ?>
         <?php while($row = $oggetto->fetch_assoc()) {?>
-            <div>
-                <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
-                <p><?php echo $row['Nome']; ?></p>
+            <div class="wrap">
+                <a href="campaign.php" onclick="window.location.href='campaign.php?Id_campagna=<?php echo urlencode($row['Id_campagna']); ?>'; return false;">
+                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Immagine']); ?>" />
+                    <p><?php echo $row['Nome']; ?></p>
+                </a>
             </div>
         <?php }?>
-        <img class="rightArrow" src="#" alt="freccia destra"/>
+        <button class="arrow dx"></button>
     </div>
+
     <script src="js/scrolling.js"></script>
 </main>
