4b06e36dc4f3d186652f79d6cedf046945848559
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-10 16:57:57 +0100

    added function signalError

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -2,3 +2,11 @@
 require_once("db/Database.php");
 $db = new Database();
 session_start();
+
+function signalError($message){
+    $template["title"] = "ERROR";
+    $template["file"] = "errorTempl.php";
+    $template["ERR_message"] = $_GET["name"] . " doesn't exist in world_id =" . $_GET["worldId"];
+    require("template/base.php");
+    exit();
+}
