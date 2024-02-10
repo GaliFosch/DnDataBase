3d8d3104adc5330013ab80cd892dcefea38bbb9d
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-10 23:18:00 +0100

    Added list of Luoghi d'interesse into state

--- app/src/template/stateTempl.php
+++ app/src/template/stateTempl.php
@@ -24,6 +24,31 @@
         <h3>Descrizione:</h3>
         <p><?php echo $state["Descrizione"] ?></p>
     </section>
+    <section>
+        <h3>Luoghi d'interesse:</h3>
+        <?php 
+        $sql="SELECT Id_luogo_d_interesse as id, Nome, Tipologia FROM Luogo_D_Interesse WHERE Stato = ? AND Mondo = ?";
+        $stmnt = $db->getConnection()->prepare($sql);
+        $stmnt->bind_param("si", $state["Nome"], $state["Id_world"]);
+        $stmnt->execute();
+        $result = $stmnt->get_result();
+        if($result->num_rows>0){
+        ?>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Tipologia</th>
+                </tr>
+                <?php while($row = $result->fetch_assoc()) {?>
+                <tr>
+                    <td><a href="?id=<?php echo $row["id"] ?>#"><?php echo $row["Nome"] ?></a></td>
+                    <td><?php echo $row["Tipologia"] ?></td>
+                </tr>
+                <?php }?>
+            </table>
+        <?php }?>
+        <a href="placeOfInterest.php?stato=<?php echo $state["Nome"] ?>&mondo=<?php echo $state["Id_mondo"]?>#"><button>Aggiungi</button></a>
+    </section>
     <footer>
         <p><?php echo $state["Creatore"] ?></p>
     </footer>
