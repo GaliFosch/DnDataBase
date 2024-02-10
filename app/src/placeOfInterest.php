0996a97dc7483f3896d04582e5e9d6679c2c00f8
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-11 00:04:23 +0100

    Added checks for user into placeOfInterest

--- app/src/placeOfInterest.php
+++ app/src/placeOfInterest.php
@@ -42,7 +42,32 @@ if(!empty($_GET["id"])){
     $lint = $result->fetch_assoc();
     $template["title"] = $lint["Nome"];
     $template["file"] = "POITempl.php";
-}else if((!empty($_GET["stato"]) && !empty($_GET["mondo"])) || !empty($_GET["app"])){
+}else if(!empty($_GET["stato"]) && !empty($_GET["mondo"])){
+    $sql = "SELECT Mondo.Creatore FROM Mondo, Stato WHERE Mondo.Id_mondo = ? AND Stato.Nome = ? AND Mondo.Id_mondo = Stato.Id_mondo";
+    $stmnt=$db->getConnection()->prepare($sql);
+    $stmnt->bind_param("is", $_GET["mondo"], $_GET["stato"]);
+    $stmnt->execute();
+    $result=$stmnt->get_result();
+    if($result->num_rows==0){
+        signalError("Stato inesistente");
+    }
+    if($result->fetch_assoc()["Creatore"]!=$_SESSION["user"]["Nickname"]){
+        signalError("Non puoi modificare questo stato");
+    }
+    $template["title"] = "Crea Luogo d'Interesse";
+    $template["file"] = "POICreationTempl.php";
+}elseif(!empty($_GET["app"])){
+    $sql = "SELECT Mondo.Creatore FROM Mondo, Luogo_D_Interesse WHERE Id_mondo = Mondo AND Id_luogo_d_interesse = ?";
+    $stmnt=$db->getConnection()->prepare($sql);
+    $stmnt->bind_param("i", $_GET["app"]);
+    $stmnt->execute();
+    $result=$stmnt->get_result();
+    if($result->num_rows==0){
+        signalError("Contenitore inesistente");
+    }
+    if($result->fetch_assoc()["Creatore"]!=$_SESSION["user"]["Nickname"]){
+        signalError("Non puoi modificare questo Contenitore");
+    }
     $template["title"] = "Crea Luogo d'Interesse";
     $template["file"] = "POICreationTempl.php";
 }
