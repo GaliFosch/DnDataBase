f63bc8cdbf5dbbd184bb66f242924019d1f60770
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-02-11 17:50:04 +0100

    state

--- app/src/template/stateCreationTempl.php
+++ app/src/template/stateCreationTempl.php
@@ -4,13 +4,13 @@
     <form action="#" method="post">
         <label for="nome">Nome:</label>
         <input type="text" name="nome" id="nome" onfocus="this.placeholder=''" onblur="this.placeholder='Il nome del tuo stato'"
-                placeholder="Il nome del tuo stato" required> <br>
+                placeholder="Il nome del tuo stato" required>
         <label for="gov">Governo:</label>
         <input type="text" name="gov" id="gov" onfocus="this.placeholder=''" onblur="this.placeholder='Il tipo di governo'"
-                placeholder="Il tipo di governo" required> <br>
+                placeholder="Il tipo di governo" required>
         <label for="architettura">Architettura:</label>
         <input type="text" name="architettura" id="architettura" onfocus="this.placeholder=''" onblur="this.placeholder='Es. Fantasy, Medioevo, Gotico, ...'"
-                placeholder="Es. Fantasy, Medioevo, Gotico, ..." required> <br>
+                placeholder="Es. Fantasy, Medioevo, Gotico, ..." required>
 
         <label for="ricchezza">Ricchezza</label>
             <select name="ricchezza" id="ricchezza" required>
@@ -32,7 +32,7 @@
             </select>
         
         <label for="desc">Descrizione:</label>
-        <textarea name="desc" id="" onfocus="this.placeholder=''" onblur="this.placeholder='Come è fatto il tuo stato?'"
+        <textarea name="desc" name="desc" id="desc" onfocus="this.placeholder=''" onblur="this.placeholder='Come è fatto il tuo stato?'"
             oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"' placeholder="Come è fatto il tuo stato?" required></textarea>
             <p class="reminder mondo">*Ricorda che la descrizione può essere visibile anche ai giocatori. Dovresti quindi descrivere
                         i dettagli principali senza dire che l'intero stato è governato da Strahd von Zarovich.
