ade1bdea9c2426af10e50c0486d96476422e7430
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-02-11 11:59:05 +0100

    Statistic by order

--- app/src/template/statTempl.php
+++ app/src/template/statTempl.php
@@ -1,27 +1,23 @@
 <main>
-    <div class="container registration">
-        <?php
-            $sql = "SELECT Classe, COUNT(*) as Count FROM Vocazione GROUP BY Classe";
-            $stmnt = $db->getConnection()->prepare($sql);
-            $stmnt->execute();
-            $result = $stmnt->get_result();
-            $map = [];
-            while ($row = $result->fetch_assoc()) {
-                echo 'Class: ' . $row['Classe'] . ', Occurrences: ' . $row['Count'] . '<br>';
-                $newItem = [];
-                $newItem['Classe'] = $row['Classe'];
-                $newItem['Count'] = $row['Count'];
-                array_push($map, $newItem);
-            }
-            $jsonString = json_encode($map);
-            ?>
+    <div class="container">
+        <div class="grid podium">
+            <?php
+                $sql = "SELECT Classe, COUNT(*) as Count 
+                        FROM Vocazione 
+                        GROUP BY Classe
+                        ORDER BY Count DESC";
+                $stmnt = $db->getConnection()->prepare($sql);
+                $stmnt->execute();
+                $result = $stmnt->get_result();
+                ?>
+                <?php while($row = $result->fetch_assoc()) {?>
+                    <div class="position">
+                        <p> <?php echo 'Class: ' . $row['Classe'] . ', Occurrences: ' . $row['Count'] . '<br>'?> </p>
+                    </div>
+                    
+                <?php }?>
+        </div>
     </div>
-
-    <script type="text/javascript">
-        var classesArray = <?php echo $jsonString; ?>;
-
-        console.log(classesArray);
-    </script>
 
     
 </main>
