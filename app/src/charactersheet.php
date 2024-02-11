d1daafd493ee919da24db6c634886e1d806bbbf5
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-02-11 18:19:21 +0100

    small changes to charactersheet

--- app/src/charactersheet.php
+++ app/src/charactersheet.php
@@ -1,7 +1,23 @@
 <?php
     require_once("bootstrap.php");
-    $template["title"] = "Character";
-    $template["file"] = "charTempl.php";
-    $template["style"] = "character.css";
+
+    if(!empty($_GET["IDPersonaggio"])){
+        $sql = "SELECT *
+                FROM Personaggio
+                JOIN Pg
+                ON Personaggio.IDPersonaggio = Pg.IDPersonaggio
+                WHERE Pg.IDPersonaggio = ?";
+        $stmnt = $db->getConnection()->prepare($sql);
+        $stmnt->bind_param("i", $_GET["IDPersonaggio"]);
+        $stmnt->execute();
+        $result = $stmnt->get_result();
+        $character = $result->fetch_assoc();
+        if(!$character){
+            signalError("Il personaggio non esiste");
+        }
+        $template["title"] = "Character";
+        $template["file"] = "charTempl.php";
+        $template["style"] = "character.css";
+    }
 
     require_once("template/base.php");
